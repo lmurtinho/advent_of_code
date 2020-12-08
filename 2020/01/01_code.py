8bbcbf1a5ce8deb5cf252d6914c5b2918c31b1c7
Lucas Murtinho <lucasmurtinho@Lucass-MacBook-Air.local>
2020-12-08 15:26:50 -0300

    First commit: AoC 2020 1-8

---
+++ 2020/01/01_code.py
@@ -0,0 +1,17 @@
+NUMBER = 2020
+with open("01_input.txt", "r") as f:
+    s = f.read()
+s = s.split('\n')[:-1]
+s = [int(i) for i in s]
+n = len(s)
+print(n)
+# found = False
+# for i in range(n):
+#     if found:
+#         break
+#     for j in range(i+1,n):
+#         if s[i] + s[j] == NUMBER:
+#             i_1, i_2 = s[i],s[j]
+#             found = True
+#             break
+# print(i_1 * i_2)
---
+++ 2020/Day_02/02_01_code.py
@@ -0,0 +1,4 @@
+with open("02_input.txt", "r") as f:
+    s = f.read()
+
+print(s)
